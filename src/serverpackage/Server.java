eac3e3f6275919fde9a651c0c5e04912320fa0d6
Olivier Eydt <olieydt@hotmail.com>
2018-08-17 14:29:06 -0400

    Phone was sometimes not receiving udp packet, added a thread that sends more than one

--- src/serverpackage/Server.java
+++ src/serverpackage/Server.java
@@ -1,9 +1,13 @@
+package serverpackage;
 import java.awt.AWTException;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -24,6 +28,10 @@ import java.net.SocketTimeoutException;
 import java.util.Enumeration;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 public class Server {
 
 	private static Robot mouseController;
@@ -33,6 +41,14 @@ public class Server {
 	private static SocketAddress multiGroup;
 	private static Socket mouseSocket;
 	private static NetworkInterface networkInterface;
+	
+	private static Thread sendIpThread = null;
+	private volatile static boolean sendIpThreadRunning = true;
+	
+	//Jframe components
+	private static JButton button;
+	private static JLabel label;
+	
 	public static void main(String[] args) throws IOException, InterruptedException{
 		//Add shutdown hook
 		Runtime.getRuntime().addShutdownHook(new Thread()
@@ -40,30 +56,85 @@ public class Server {
 		    @Override
 		    public void run()
 		    {
+		        System.out.print("\nShutting down server");
+		        resetThread();
 		        closeAllSockets();
+		        typewriterPrint("...");
+		        System.out.println("Good bye.");
 		    }
 		});
-		
+		//make window
+		createJFrame();
+	}
+	
+	private static void createJFrame() {
+		JFrame frame = new JFrame("LaptorControlServer");
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+		    @Override
+		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+		    	//print in window
+		    	System.exit(0);
+		    }
+		});
+		//add buttons
+		frame.getContentPane().setLayout(new FlowLayout());
+		label = new JLabel("No devices found on network.");
+		frame.getContentPane().add(label);
+		button = new JButton("Connect");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(button.getText().equals("Connect")) {
+					//Get client address, start multicast
+					label.setText("Looking for devices...");
+					Thread t = new Thread(new Runnable() {
+						public void run() {
+							try {
+								button.setEnabled(false);
+								startConnectionSequence();
+							} catch (AWTException e) {
+							} catch(IOException e) {
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					});
+					t.start();
+				} else {
+					resetThread();
+					closeAllSockets();
+				}
+			}
+		});
+		frame.getContentPane().add(button);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private static void typewriterPrint(String strToPrint) {
 		try {
-			//Get client address, start multicast
-			startConnectionSequence();
-		} catch (AWTException e) {
-			e.printStackTrace();
-		} catch(IOException e) {
-			e.printStackTrace();
+			int i;
+			for(i=0; i<strToPrint.length()-1; i++) {
+				System.out.print(strToPrint.charAt(i));
+				Thread.sleep(400);
+			}
+			System.out.println(strToPrint.charAt(i));
+		} catch (InterruptedException e) {
+			
 		}
 	}
 	
 	private static void closeAllSockets() {
 		if(serverSocket != null && !serverSocket.isClosed()) {
-	    		try {
+	    	try {
 				serverSocket.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	    if(multiSocket != null && !multiSocket.isClosed()) {
-	    		try {
+	    	try {
 				multiSocket.leaveGroup(multiGroup, networkInterface);
 				multiSocket.close();
 			} catch (IOException e) {
@@ -72,30 +143,38 @@ public class Server {
 			}
 	    }
 	    if(mouseSocket != null && !mouseSocket.isClosed()) {
-	    		try {
+	    	try {
 				mouseSocket.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	    }
+	    label.setText("No device found.");
+	    button.setText("Connect");
 	}
 
-	private static void sendLocalAddress(InetAddress clientAddress) throws IOException {
+	private static void sendLocalAddress(InetAddress clientAddress) throws IOException, InterruptedException {
 		DatagramSocket socket = new DatagramSocket();
 		socket.setSoTimeout(5000);
 		DatagramPacket sendPacket = new DatagramPacket("PCIP".getBytes(), "PCIP".getBytes().length, clientAddress, 12346);
-		socket.send(sendPacket);
+		while(sendIpThreadRunning) {
+			socket.send(sendPacket);
+			Thread.sleep(300);
+		}
 		socket.close();
 	}
 
-	private static void startController(InetAddress client) throws AWTException, IOException {
+	private static void startController(InetAddress client) throws AWTException, IOException, InterruptedException {
 		mouseController = new Robot();
 		serverSocket = new ServerSocket(12347);
 		serverSocket.setSoTimeout(10000);
 		while(true) {
 			try {
 				mouseSocket = serverSocket.accept();
+				//received a command from phone, stop sending ip address
+				sendIpThreadRunning = false;
+				sendIpThread.join();
 				//System.out.println("Connected");
 				DataInputStream input;
 				byte[] received = new byte[1024];
@@ -159,7 +238,7 @@ public class Server {
 		return packet.getAddress()*/
 	}
 
-	private static void controlMouse(String mouseCommand) throws IOException, AWTException {
+	private static void controlMouse(String mouseCommand) throws IOException, AWTException, InterruptedException {
 		if(mouseController != null) {
 			if(mouseCommand.trim().equals("click")) {
 				//apply click
@@ -167,6 +246,7 @@ public class Server {
 			    mouseController.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 			} else if(mouseCommand.trim().equals("end")) {
 				closeAllSockets();
+				resetThread();
 				startConnectionSequence();
 			} else {
 				//move mouse by deltaX and deltaY from finger move on phone
@@ -183,9 +263,35 @@ public class Server {
 		}
 	}
 
-	private static void startConnectionSequence() throws IOException, AWTException {
+	private static void resetThread() {
+		if(sendIpThreadRunning && sendIpThread != null) {
+			sendIpThreadRunning = false;
+			try {
+				sendIpThread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		sendIpThreadRunning = true;
+	}
+
+	private static void startConnectionSequence() throws IOException, AWTException, InterruptedException {
 		InetAddress client = findClient();
-		sendLocalAddress(client);
+		sendIpThread = new Thread("Ip Thread") {
+			public void run() {
+				try {
+					sendLocalAddress(client);
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		sendIpThread.start();
+		label.setText("Device found!");
+		button.setText("Disconnect");
+		button.setEnabled(true);
 		startController(client);
 	}
 
